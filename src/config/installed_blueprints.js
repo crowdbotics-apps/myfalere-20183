af9cfc4a4b43f5969b24f79a386585952492f693
Crowdbotics <team@crowdbotics.com>
2020-09-10 14:10:06 +0000

    add package blueprint-react-native-message-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Messaging8100152', human_name: 'Messaging 8', access_route: 'Messaging8100152', icon: 'comments'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
