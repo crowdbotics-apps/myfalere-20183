9dc09a8f3b7e1e898937d4ba89ebb5632fcf913c
Crowdbotics <team@crowdbotics.com>
2020-09-10 14:10:37 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile4100156', human_name: 'User Profile 4', access_route: 'UserProfile4100156'},
 { name: 'EmailAuth5100155', human_name: 'Email Auth 5', access_route: 'EmailAuth5100155', icon: 'envelope-o'},
 { name: 'Dashboard16100154', human_name: 'Dashboard 1 6', access_route: 'Dashboard16100154'},
 { name: 'NotificationList7100153', human_name: 'Notification List 7', access_route: 'NotificationList7100153'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile4100156Navigator from '../features/UserProfile4100156/navigator';
 import EmailAuth5100155Navigator from '../features/EmailAuth5100155/navigator';
 import Dashboard16100154Navigator from '../features/Dashboard16100154/navigator';
 import NotificationList7100153Navigator from '../features/NotificationList7100153/navigator';
@@ -17,6 +18,7 @@ import Messaging8100152Navigator from '../features/Messaging8100152/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+UserProfile4100156: { screen: UserProfile4100156Navigator },
 EmailAuth5100155: { screen: EmailAuth5100155Navigator },
 Dashboard16100154: { screen: Dashboard16100154Navigator },
 NotificationList7100153: { screen: NotificationList7100153Navigator },
